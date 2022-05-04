b373f0bb065113d3166fd0bcf230d4bb4d776e46
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-05-04 15:52:46 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (25.55%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,31 +1,57 @@
 class Solution {
 public:
-    // brute force
-    void sortColors(vector<int>& nums) {
-        int N = nums.size(),z=0,o=0,t=0;
-        for(auto num : nums){
-            if(num == 0){
-                z++;
-            }else if(num == 1){
-                o++;
-            }else{
-                t++;
-            }
-        }
+    // optimized approach: dutch national flag algorithm
+    void sortColors(vector<int> &nums){
+        int N = nums.size();
+        int low = 0;
+        int hi = N-1;
+        int mid = low;
         
-        int i = 0;
-        while(z--){
-            nums[i] = 0;
-            i++;
-        }
-        while(o--){
-            nums[i] = 1;
-            i++;
-        }
-        while(t--){
-            nums[i] = 2;
-            i++;
+        // the logic is that 
+        // 0 from 0-index to lo-1
+        // 1 from lo-index to mid-1
+        // 2 from hi-index to N-1
+        
+        while(mid <= hi){
+            if(nums[mid] == 0){
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[hi]);
+                hi--;
+            }
         }
         return;
     }
+    // brute force
+//     void sortColors(vector<int>& nums) {
+//         int N = nums.size(),z=0,o=0,t=0;
+//         for(auto num : nums){
+//             if(num == 0){
+//                 z++;
+//             }else if(num == 1){
+//                 o++;
+//             }else{
+//                 t++;
+//             }
+//         }
+        
+//         int i = 0;
+//         while(z--){
+//             nums[i] = 0;
+//             i++;
+//         }
+//         while(o--){
+//             nums[i] = 1;
+//             i++;
+//         }
+//         while(t--){
+//             nums[i] = 2;
+//             i++;
+//         }
+//         return;
+//     }
 };
