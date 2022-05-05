35046581516ff2b528b4401f2e5f8fc19896db45
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-05-05 10:43:33 +0530

    Time: 20 ms (47.62%), Space: 13.1 MB (82.58%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -5,24 +5,79 @@ public:
         int N = matrix.size();
         int M = matrix[0].size();
         
-        vector<int> row(N,-1);
-        vector<int> col(M,-1);
+        int firstRow = 1,firstCol = 1;
+        
+        // check for zero in first col
         for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
+            if(matrix[i][0] == 0){
+                firstCol = 0;
+            }
+        }
+        // check for zero in first row
+        for(int j = 0; j < M; j++){
+            if(matrix[0][j] == 0){
+                firstRow = 0;
+            }
+        }
+        
+        // mark first row and first col of matrix as markers
+        for(int i = 1; i < N; i++){
+            for(int j = 1; j < M; j++){
                 if(matrix[i][j] == 0){
-                    row[i] = col[j] = 0;
+                    matrix[0][j] = matrix[i][0] = 0;
                 }
             }
         }
         
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(row[i] == 0 || col[j] == 0){
+        // corresponding to markers set zero in whole matrix
+        for(int i = 1; i < N; i++){
+            for(int j = 1; j < M; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
+        
+        // set first row on the basis of firstRow
+        if(firstRow == 0){
+            for(int j = 0; j < M; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        // set first col on the basis of firstCol
+        if(firstCol == 0){
+            for(int i = 0; i < N; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        return;
     }
+//     TC: O(MN + MN) SC:O(M+N)
+//     void setZeroes(vector<vector<int>> &matrix){
+//         int N = matrix.size();
+//         int M = matrix[0].size();
+        
+//         vector<int> row(N,-1);
+//         vector<int> col(M,-1);
+//         for(int i = 0; i < N; i++){
+//             for(int j = 0; j < M; j++){
+//                 if(matrix[i][j] == 0){
+//                     row[i] = col[j] = 0;
+//                 }
+//             }
+//         }
+        
+//         for(int i = 0; i < N; i++){
+//             for(int j = 0; j < M; j++){
+//                 if(row[i] == 0 || col[j] == 0){
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//     }
+    
+    
 // brute force :
 //     TC:O(MN + MN + (M+N))
     
