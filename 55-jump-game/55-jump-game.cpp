2a709758dc88de2f69348e119ab9ae811c442f0f
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-05-13 20:03:22 +0530

    Time: 79 ms (68.20%), Space: 48.2 MB (99.64%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,17 +2,33 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> dp(n,false);
-        dp[n-1] = true;
+        int maxIndexReached = 0;
         
-        for(int i = n - 2; i >= 0; i--){
-            for(int j = 1; j <= nums[i] && i + j < n; j++){
-                if(dp[i+j] == true){
-                    dp[i] = true;
-                    break;
-                }
+        if(n == 1){
+            return true;
+        }
+        for(int i = 0; i < n-1 && maxIndexReached >= i; i++){
+            maxIndexReached = max(maxIndexReached,
+                                 i + nums[i]);
+            if(maxIndexReached >= n-1){
+                return true;
             }
         }
-        return dp[0];
+        return false;
     }
 };
+
+// DP: TC = O(n*m) SC = O(n)
+//  int n = nums.size();
+//         vector<bool> dp(n,false);
+//         dp[n-1] = true;
+        
+//         for(int i = n - 2; i >= 0; i--){
+//             for(int j = 1; j <= nums[i] && i + j < n; j++){
+//                 if(dp[i+j] == true){
+//                     dp[i] = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp[0];
