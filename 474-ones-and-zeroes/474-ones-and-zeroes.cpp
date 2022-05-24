84934e14eebd7bb746185c1af097cc44d247fb48
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-05-24 13:06:52 +0530

    Time: 444 ms (40.33%), Space: 103.2 MB (6.24%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int size = strs.size();
-        vector<vector<vector<int>>> dp(size + 1,vector<vector<int>>(m+1,vector<int>(n+1,0)));
+        vector<vector<int>> prevIndex(m+1,vector<int>(n+1,0));
         
         for(int index = 1; index <= size; index++){
-            int countZero = count(strs[index-1].begin(),strs[index-1].end(),'0');
-            int countOne = strs[index-1].size() - countZero;
+            int countZeros = count(strs[index-1].begin(),strs[index-1].end(),'0');
+            int countOnes = strs[index-1].size() - countZeros;
             
+            vector<vector<int>> currIndex(m+1,vector<int>(n+1,0));
             for(int zero = 0; zero <= m; zero++){
                 for(int one = 0; one <= n; one++){
-                    int notTake = dp[index-1][zero][one];
+                    
                     int take = 0;
-                    if(countZero <= zero && countOne <= one){
-                        take = 1 + dp[index-1][zero-countZero][one-countOne];
+                    int notTake = prevIndex[zero][one];
+                    if(zero-countZeros >= 0 && one-countOnes >= 0){
+                        take = 1 + prevIndex[zero-countZeros][one-countOnes];
                     }
                     
-                    dp[index][zero][one] = max(take,notTake);
+                    currIndex[zero][one] = max(take,notTake);
                 }
             }
+            prevIndex = currIndex;
         }
-        return dp[size][m][n];
+        return prevIndex[m][n];
     }
 };
