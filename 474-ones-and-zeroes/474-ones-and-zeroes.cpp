9b47bccd5d9b64c8873728bb447774cda6877f4c
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-05-24 12:57:29 +0530

    Time: 415 ms (44.62%), Space: 102.9 MB (11.83%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,28 +1,25 @@
 class Solution {
-    vector<vector<vector<int>>> dp;
-    int helper(vector<string> &strs,int maxZeros, int maxOnes, int index){
-        if(index == strs.size()){
-            return 0;
-        }
-        if(dp[index][maxZeros][maxOnes] != -1){
-            return dp[index][maxZeros][maxOnes];
-        }
-        
-        int take = 0,notTake = 0;
-        notTake = helper(strs,maxZeros,maxOnes,index+1);
-        
-        int countZeros = count(strs[index].begin(),strs[index].end(),'0');
-        int countOnes = strs[index].size() - countZeros;
-        
-        if(countZeros <= maxZeros && countOnes <= maxOnes){
-            take = 1 + helper(strs,maxZeros-countZeros,maxOnes-countOnes,index+1);
-        }
-        return dp[index][maxZeros][maxOnes] = max(take,notTake);
-    }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int size = strs.size();
-        dp.resize(size,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
-        return helper(strs,m,n,0);
+        vector<vector<vector<int>>> dp(size + 1,vector<vector<int>>(m+1,vector<int>(n+1,0)));
+        
+        for(int index = 1; index <= size; index++){
+            int countZero = count(strs[index-1].begin(),strs[index-1].end(),'0');
+            int countOne = strs[index-1].size() - countZero;
+            
+            for(int zero = 0; zero <= m; zero++){
+                for(int one = 0; one <= n; one++){
+                    int notTake = dp[index-1][zero][one];
+                    int take = 0;
+                    if(countZero <= zero && countOne <= one){
+                        take = 1 + dp[index-1][zero-countZero][one-countOne];
+                    }
+                    
+                    dp[index][zero][one] = max(take,notTake);
+                }
+            }
+        }
+        return dp[size][m][n];
     }
 };
