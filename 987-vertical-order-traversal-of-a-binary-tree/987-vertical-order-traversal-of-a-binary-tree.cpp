1cde1d31e09c7deac0fa851675e0c4728d2d9c2d
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-05-24 18:18:53 +0530

    Time: 4 ms (79.48%), Space: 12.4 MB (72.11%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int> > ans;
+
+        if(!root){
+            return ans;
+        }
+        
+//      map<horizontal Distance,map<level,vector<integer value > > >
+        map<int,map<int,vector<int> > > nodes;
+//                          horizon.. level..
+        queue<pair<TreeNode*,pair<int,int> > > q;
+        
+        q.push(make_pair(root,make_pair(0,0)));
+        
+        while(q.empty() == false){
+            auto front = q.front();
+            q.pop();
+            
+            auto node = front.first;
+            auto hDistance = front.second.first;
+            auto level = front.second.second;
+            
+            nodes[hDistance][level].push_back(node->val);
+            
+            if(node->left){
+                q.push(make_pair(node->left,make_pair(hDistance-1,level+1)));
+            }
+            if(node->right){
+                q.push(make_pair(node->right,make_pair(hDistance+1,level+1)));
+            }
+        }
+        
+        for(auto i : nodes){
+            vector<int>res;
+            for(auto j : i.second){
+                sort(j.second.begin(),j.second.end());
+                for(auto k : j.second){
+                    res.push_back(k);
+                }
+                
+            }
+            ans.push_back(res);
+        }
+        return ans;        
+    }
+};
