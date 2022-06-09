5961744c0affaa8a4f09d940b15d015e64f736bb
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-06-09 20:55:09 +0530

    Time: 10 ms (94.54%), Space: 15.6 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -8,18 +8,26 @@ public:
             return ans;
         }
         
-        int i = 1, j = n;
-        while(i < j){
-            if(numbers[i-1] + numbers[j-1] == target){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
-            else if(numbers[i-1] + numbers[j-1] < target){
-                i++;
-            }
-            else{
-                j--;
+        int i = 0;
+        for(i = 0; i < n-1; i++){
+            
+            int newTarget = target - numbers[i];
+            int lo = i+1;
+            int hi = n-1;
+                       
+            while(lo <= hi){
+                int mid = (lo + hi)/2;
+                if(numbers[mid] == newTarget){
+                    ans.push_back(i+1);
+                    ans.push_back(mid+1);
+                    return ans;
+                }
+                else if(numbers[mid] < newTarget){
+                    lo = mid+1;
+                }
+                else{
+                    hi = mid - 1;
+                }
             }
         }
         return ans;
