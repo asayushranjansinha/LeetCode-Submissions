a351c769bae0c246b812bc1b4f076473e3700b51
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-08-29 19:59:51 +0530

    Time: 42 ms (40.84%), Space: 19.3 MB (57.51%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> resultant;
         int N = nums.size();
+        int nonZeroIndex = 0;
         
         for(int i = 0; i < N; i++){
             if(nums[i] != 0){
-                resultant.push_back(nums[i]);
+                swap(nums[nonZeroIndex++],nums[i]);
             }
         }
-        for(int i = 0; i < N; i++){
-            if(nums[i] == 0){
-                resultant.push_back(0);
-            }
-        }
-        nums = resultant;
+        return;
     }
 };
