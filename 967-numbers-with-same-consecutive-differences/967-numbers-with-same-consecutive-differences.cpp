eed25a00a08f36563cfab6552144c74716580d69
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-09-05 22:21:38 +0530

    Time: 12 ms (10.67%), Space: 7.4 MB (76.71%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,56 +1,31 @@
-/* 
-    First of all understand the question properly:
-*   N is the total number of digits each number could have
-*   K is the common difference(absolute) b/w consecutive digits
-*   For every place value, we could only have digits 0 - 9 excluding leading 0s.
-*   No place can have 10 or more in place of a single digit.
-*   The most significant digit can only be b/w 1 to 9
-
-    Approach building
-    Since we have a limit of choosing the most significant digit, we will loop from 1 to 9 and try to create as many numbers as possible.
-    Now for every place value in a particular number we have certain conditions to obey,
-    but we have two choices, 1. add K to previous digit or subtract K from previous digit to have a difference of K. So we try both and whatever suits we add into result array.
-*/
 class Solution {
-private:
-    void createNumber(int numberOfDigits, int numberSoFar, int K,
-                     vector<int>& result){
-        if(numberOfDigits == 0){
-            // we have succesfully created a number
-            // add to result array
-            result.push_back(numberSoFar);
-            return;
-        }
-        // take out last digit till now
-        int lastDigit = numberSoFar % 10;
-        
-        // create a difference of K
-        int addcase = lastDigit + K;
-        int subcase = lastDigit - K;
-        
-        // check validity of both cases
-        if(addcase <= 9){
-            int modifiedNumber = numberSoFar * 10 + addcase;
-            createNumber(numberOfDigits-1,modifiedNumber,K,result);
-        }
-//         K > 0 is needed to handle a case when K = 0 so we do not end up creating same number twice since addcase and subcase wil be same
-        if(K > 0 && subcase >= 0){
-            int modifiedNumber = numberSoFar * 10 + subcase;
-            createNumber(numberOfDigits-1,modifiedNumber,K,result);
-        }
-//         TC: O(N* 2 ^N) SC: O(N) recursion
-    }
 public:
-    vector<int> numsSameConsecDiff(int N, int K) {
-        vector<int> result;
-        if(N == 1) result.push_back(0);
+//     optimized approach
+    vector<int> numsSameConsecDiff(int n, int k) {
+        // most significant digits are fixed from 1 to 9
+        vector<int>result = {1,2,3,4,5,6,7,8,9};
         
-        // choosing most significant digit
-        for(int i = 1; i <= 9; i++){
-            int numberOfDigitsLeft = N-1;
-            int numberSoFar = i;
-            
-            createNumber(numberOfDigitsLeft,numberSoFar,K,result);
+        // no of digits in each number can be n
+        // but we already have selected most significant digits so n - 1
+        
+        // creating all possible combination, digit by digit
+        for(int i = n - 1; i > 0; i--){
+            vector<int> tempResult;
+            for(auto numberSoFar : result){
+                int lastDigit = numberSoFar % 10;
+                int addCase = lastDigit + k;
+                int subCase = lastDigit - k;
+                
+                if(addCase <= 9){
+                    int modNumber = numberSoFar * 10 + addCase;
+                    tempResult.push_back(modNumber);
+                }
+                if(k > 0 && subCase >= 0){
+                    int modNumber = numberSoFar * 10 + subCase;
+                    tempResult.push_back(modNumber);
+                }
+            }
+            result = tempResult;
         }
         return result;
     }
