d66a5c08338398eca26686558cfcf464187d3821
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-09-16 18:23:10 +0530

    Time: 821 ms (31.85%), Space: 141.1 MB (77.69%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-     vector<int> findOriginalArray(vector<int>& A) {
-        if (A.size() % 2) return {};
-        unordered_map<int, int> c;
-        for (int a : A) c[a]++;
-        vector<int> keys;
-        for (auto it : c)
-            keys.push_back(it.first);
-        sort(keys.begin(), keys.end(), [](int i, int j) {return abs(i) < abs(j);});
-        vector<int> res;
-        for (int x : keys) {
-            if (c[x] > c[2 * x]) return {};
-            for (int i = 0; i < c[x]; ++i, c[2 * x]--)
-                res.push_back(x);
+    vector<int> findOriginalArray(vector<int>& changed) {
+        int N = changed.size();
+        
+        if(N % 2) return {};
+        
+        unordered_map<int,int> mpp;
+        vector<int> original;
+        
+        for(auto it : changed){
+            mpp[it]++;
         }
-        return res;
+        
+        sort(changed.begin(),changed.end());
+        for(int i = 0; i < N; i++){
+            int num1 = changed[i];
+            int freq1 = mpp[num1];
+            
+            if(freq1 == 0) continue;
+            
+            if(num1 == 0 && freq1 % 2) return {};
+            
+            int num2 = num1 * 2;
+            int freq2 = mpp[num2];
+            
+            if(freq1 > freq2) return {};
+            
+            while(mpp[num1] > 0){
+                original.push_back(num1);
+                mpp[num2]--;
+                mpp[num1]--;
+            }
+        }
+        return original;
     }
 };
