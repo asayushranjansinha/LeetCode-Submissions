e7d5c536f31d689241f4d1f3ec06603ff7060c1c
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2022-09-17 19:27:48 +0530

    Time: 292 ms (99.05%), Space: 119.2 MB (73.28%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int rec(int i,int l,int h,vector<int>&nums,vector<int>&m,vector<vector<int>>&dp){
-        if(i>=m.size()){
-            return 0;
-        }
-        if(l>h){
-            return 0;
-        }
-        if(dp[i][l]!=-1001)
-            return dp[i][l];
+    int maximumScore(vector<int>& nums, vector<int>& mul) {
+        int N = nums.size();
+        int M = mul.size();
         
-        int take=rec(i+1,l+1,h,nums,m,dp)+nums[l]*m[i];
-        int notTake=rec(i+1,l,h-1,nums,m,dp)+nums[h]*m[i];
-        
-        return dp[i][l]=max(take,notTake);
-    }
-    int maximumScore(vector<int>& nums, vector<int>& m) {
-        int n=nums.size();
-        vector<vector<int>>dp(m.size(),vector<int>(m.size(),-1001));
-        return rec(0,0,n-1,nums,m,dp);
+        vector<int>nextRow(M+1,0);
+        for(int index = M - 1; index >= 0; index--){
+            vector<int>currRow(M+1,0);
+            for(int left = index; left >= 0; left--){
+                int leftSel = nums[left] * mul[index] + nextRow[left+1];
+                int rightSel = nums[N - 1 -(index - left)] * mul[index] + nextRow[left];
+                currRow[left] = max(leftSel,rightSel);
+            }
+            nextRow = currRow;
+        }
+        return nextRow[0];
     }
 };
